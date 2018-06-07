b82a0cd2302e5d39f1714fc3f10a8c45815c8560
Paige Boyer <paige1381@gmail.com>
2018-06-07 15:42:30 -0700

    UI mocked up

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send("Hello!");
+  res.send("Root route!");
 });
 
-const PORT = process.env.PORT || 3000;
+app.get('/hello/:firstname/:lastname', (req, res) => {
+  console.log(req.params);
+  res.send("Hello " + req.params.firstname + " " + req.params.lastname);
+});
+
+app.get('/hello/:firstname', (req, res) => {
+  console.log(req.params);
+  console.log(req.query);
+  res.send("Hello " + req.query.title + " " + req.params.firstname);
+});
+
+
 
 app.listen(PORT, console.log('listening on port', PORT));
