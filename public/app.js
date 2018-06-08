156565444062e0d85913637d83010756311abb7e
Paige Boyer <paige1381@gmail.com>
2018-06-07 18:15:39 -0700

    first round letter displayed

--- public/app.js
+++ public/app.js
@@ -1,25 +1,28 @@
-$(() => {
-
   const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O','P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
   let gameWords = ['RABBIT', 'BUNNY', 'CARROT', 'LETTUCE', 'BURROW', 'FLUFFY', 'FLOPPY', 'LITTER', 'PELLETS']
 
+  $(() => {
+
+  let currentWord = gameWords[Math.floor(Math.random() * (gameWords.length))];
+  let currentLetter = null;
   let guesses = 8;
 
-
-  const createKeyboard = () => {
-    for (let i = 0; i < alphabet.length; i++) {
-      const $key = $('<div>').addClass('key').text(alphabet[i]);
-      $('.keyboard').append($key);
+  const createBlanks = () => {
+    // let currentWord = gameWords[Math.floor(Math.random() * (gameWords.length))];
+    console.log(currentWord);
+    for (let i = 0; i < currentWord.length; i++) {
+        const $blank = $('<div>').addClass('blank');
+        $('.blanks').append($blank);
     }
   }
 
-  const createBlanks = () => {
-    let word = gameWords[Math.floor(Math.random() * (gameWords.length))];
-    console.log(word);
-    for (let i = 0; i < word.length; i++) {
-        const $blank = $('<div>').addClass('blank');
-        $('.blanks').append($blank);
+  const createKeyboard = () => {
+    console.log(currentWord);
+    for (let i = 0; i < alphabet.length; i++) {
+      const $key = $('<div>').addClass('key').text(alphabet[i]);
+      $('.keyboard').append($key);
+      $key.on('click', setKeyAction);
     }
   }
 
@@ -27,10 +30,36 @@ $(() => {
     $('.guesses').text(guesses + " guesses left!")
   }
 
+  const setKeyAction = (event) => {
+    const $key = $(event.currentTarget);
+    currentLetter = $key.text();
+    console.log(currentLetter);
+    checkLetterMatch();
+  }
 
-createKeyboard();
-createBlanks();
-createGuesses();
+  const checkLetterMatch = () => {
+    let currentWordArr = currentWord.split("");
+    console.log(currentWord);
+    for (let i = 0; i < currentWordArr.length; i++) {
+      if (currentWordArr[i] === currentLetter) {
+        addLetter(i);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  const addLetter = (n) => {
+    console.log(currentLetter);
+    console.log(n);
+    console.log($('.blank').eq(n));
+    const $blank = $('.blank').eq(n);
+    $blank.text(currentLetter);
+  }
+
+  createBlanks();
+  createKeyboard();
+  createGuesses();
 
 
 
